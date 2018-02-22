8e3f48ef6c083bad01f9b668950c4190786fa288
VjacheslavZ <slavikov.net@mail.ru>
2018-02-22 14:42:51 +0200

    mongoose shema start

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-var express = require('express');
-var app = express(),
+var express = require('express'),
+    app = express(),
     bodyParser = require('body-parser'),
     path = require('path'),
     logger = require('morgan'),
-    cookieParser = require('cookie-parser'),
-    index = require('./routes/index'),
     port = 8080,
-    fs = require("fs");
+    mongoose = require('mongoose'),
+    session = require('express-session'),
+    MongoStore = require('connect-mongo')(session);
 
-// var users = require('./routes/users');
-const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
@@ -17,9 +15,88 @@ mongoose.connect('mongodb://localhost/myApp', {})
     .then(() => console.log('MongoDB started'))
     .catch( e => console.log('mongoDB error' + e));
 
-require('./person.model');
+app.use(bodyParser());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const Person = mongoose.model('persons');
+app.get('/users', function (req, res) {
+    res.send("users");
+});
+
+// include routes
+var routes = require('./routes/router');
+app.use('/', routes);
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', index);
+
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(port, function () {
+    console.log("API app started on port " + port)
+});
+
+module.exports = app;
+
+
+
+// require('./person.model');
+
+
+// var users = [
+//     {
+//         id: "x",
+//         name: "1",
+//         password: "2",
+//         confirmPassword: "3",
+//         email: "4"
+//     },
+//     {
+//         name: "11",
+//         password: "22",
+//         confirmPassword: "33",
+//         mail: "44"
+//     }
+// ];
+
+// app.post('/register', function (req, res) {
+//
+//     if(!req.body) return res.sendStatus(400);
+//
+//
+//     var newUersData = {
+//         userName: req.body.userName,
+//         userPassword: req.body.userPassword,
+//         confirmPassword: req.body.confirmPassword,
+//         userEmail: req.body.userEmail,
+//     };
+//
+//     // users.push(newUersData);
+//     res.send(newUersData);
+//
+//     console.log(newUersData);
+//     // res.json(req.body);
+// });
+
+//const Person = mongoose.model('persons');
 
 // const person = new Person({
 //     name: 'first user',
@@ -62,86 +139,6 @@ const Person = mongoose.model('persons');
 // .forEach(p => {
 //    new Person(p).save()
 // });
-
-
-app.use(bodyParser());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get('/users', function (req, res) {
-    res.send("users");
-});
-
-var users = [
-    {
-        id: "x",
-        name: "1",
-        password: "2",
-        confirmPassword: "3",
-        email: "4"
-    },
-    {
-        name: "11",
-        password: "22",
-        confirmPassword: "33",
-        mail: "44"
-    }
-];
-
-app.post('/register', function (req, res) {
-
-    if(!req.body) return res.sendStatus(400);
-
-
-    var newUersData = {
-        userName: req.body.userName,
-        userPassword: req.body.userPassword,
-        confirmPassword: req.body.confirmPassword,
-        userEmail: req.body.userEmail,
-    };
-
-    // users.push(newUersData);
-    res.send(newUersData);
-
-    console.log(newUersData);
-    // res.json(req.body);
-});
-
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-// app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-app.listen(port, function () {
-    console.log("API app started on port " + port)
-});
-
-module.exports = app;
 
 
 
--- public/js/index.js
+++ public/js/index.js
@@ -1,25 +1,25 @@
-$("form").submit(function(e) {
-    e.preventDefault();
-
-    var registerForm = document.forms["registerForm"],
-        userName = registerForm.elements["userName"].value,
-        userPassword = registerForm.elements["userPassword"].value,
-        confirmPassword = registerForm.elements["confirmPassword"].value,
-        userEmail = registerForm.elements["userEmail"].value;
-
-    $.ajax({
-        type: "POST",
-        url: "/register",
-        data: JSON.stringify({
-            userName: userName,
-            userPassword: userPassword,
-            confirmPassword: confirmPassword,
-            userEmail: userEmail
-        }),
-        dataType: "json",
-        contentType: "application/json",
-        success: function(data){
-            console.log(data);
-        },
-    });
-});
+// $("form").submit(function(e) {
+//     e.preventDefault();
+//
+//     var registerForm = document.forms["registerForm"],
+//         userName = registerForm.elements["userName"].value,
+//         userPassword = registerForm.elements["userPassword"].value,
+//         confirmPassword = registerForm.elements["confirmPassword"].value,
+//         userEmail = registerForm.elements["userEmail"].value;
+//
+//     $.ajax({
+//         type: "POST",
+//         url: "/register",
+//         data: JSON.stringify({
+//             userName: userName,
+//             userPassword: userPassword,
+//             confirmPassword: confirmPassword,
+//             userEmail: userEmail
+//         }),
+//         dataType: "json",
+//         contentType: "application/json",
+//         success: function(data){
+//             console.log(data);
+//         },
+//     });
+// });
